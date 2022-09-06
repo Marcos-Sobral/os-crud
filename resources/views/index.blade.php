b7523349d17c075bca1ae1871931e1af70909f78
Marcos-Sobral <vini15oficial@gmail.com>
2022-09-06 16:47:46 -0300

    :lipstick: S1-feat: Estilizaçao CSS view index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,16 +2,139 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>oi</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/style.css'); }} " media="screen" />
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/header.css'); }} " media="screen" />
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/normalizer.css'); }} " media="screen" />
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/footer.css'); }} " media="screen" />
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/responsive.css'); }} " media="screen" />
+    <script src="https://kit.fontawesome.com/7d7b31a9bc.js" crossorigin="anonymous"></script>
+    <title>Selfcare - home</title>
 </head>
 <body>
-    <nav>
-        <a href="">get</a>
-        <ul>
-            <li></li>
-        </ul>
-    </nav>
+    <header>
+        <nav id="nav">
+            <a href="" id="nav-title-icon"><i class="fa-solid fa-suitcase"></i>Minhas Malas</a>
+            <ul id="nav-menu-ul">
+                <li id="nav-menu-li"><a href=""></a><i class="fa-solid fa-magnifying-glass"></i></li>
+                <li id="nav-menu-li"><a href=""></a><i class="fa-solid fa-user"></i></li>
+                <li id="nav-menu-li"><a href=""></a><i class="fa-solid fa-cart-shopping"></i></li>
+            </ul>
+        </nav>
+    </header>
+        <section>
+            <figure>
+                <img src="{{ URL::asset('assets/img/Mini Perfumes/Mercado Shop desktop.png'); }}" alt="clique aqui" id="banner">
+            </figure>
+        </section>
+    <main>
+        <div id="title-menu">
+            <h3>Promoção</h3>
+            <h3>9 Produtos</h3>
+        </div>
+        <hr>
+        <div id="grid-container">
+            <div id="grid-item-main-4">
+                <img src="{{ URL::asset('assets/img/Mini Perfumes/A01.png'); }}" alt="" id="img-main">
+                <div id="mt-20">
+                    Lorem ipsum dolor, sit
+                    <p>R$<b id="title-preco-main"> 50,00</b></p>
+                </div>
+            </div>
+            <div id="grid-item-main-5">
+                <img src="{{ URL::asset('assets/img/Mini Perfumes/A02.png'); }}" alt="" id="img-main">
+                <div id="mt-20">
+                    Lorem ipsum dolor, sit
+                    <p>R$<b id="title-preco-main"> 50,00</b></p>
+                </div>
+            </div>
+            <div id="grid-item-main-6">
+                <img src="{{ URL::asset('assets/img/Mini Perfumes/A02.png'); }}" alt="" id="img-main">
+                <div id="mt-20">
+                    Lorem ipsum dolor, sit
+                    <p>R$<b id="title-preco-main"> 50,00</b></p>
+                </div>
+            </div>
+
+
+            <div id="grid-item-main-4">
+                <img src="{{ URL::asset('assets/img/Mini Perfumes/A04.png'); }}" alt="" id="img-main">
+                <div id="mt-20">
+                    Lorem ipsum dolor, sit
+                    <p>R$<b id="title-preco-main"> 50,00</b></p>
+                </div>
+            </div>
+            <div id="grid-item-main-5">
+                <img src="{{ URL::asset('assets/img/Mini Perfumes/A05.png'); }}" alt="" id="img-main">
+                <div id="mt-20">
+                    Lorem ipsum dolor, sit
+                    <p>R$<b id="title-preco-main"> 50,00</b></p>
+                </div>
+            </div>
+            <div id="grid-item-main-6">
+                <img src="{{ URL::asset('assets/img/Mini Perfumes/A06.png'); }}" alt="" id="img-main">
+                <div id="mt-20">
+                    Lorem ipsum dolor, sit
+                    <p>R$<b id="title-preco-main"> 50,00</b></p>
+                </div>
+            </div>
+
+
+            <div id="grid-item-main-7">
+                <img src="{{ URL::asset('assets/img/Mini Perfumes/A07.png'); }}" alt="" id="img-main">
+                <div id="mt-20">
+                    Lorem ipsum dolor, sit
+                    <p>R$<b id="title-preco-main"> 50,00</b></p>
+                </div>
+            </div>
+            <div id="grid-item-main-8">
+                <img src="{{ URL::asset('assets/img/Mini Perfumes/A08.png'); }}" alt="" id="img-main">
+                <div id="mt-20">
+                    Lorem ipsum dolor, sit
+                    <p>R$<b id="title-preco-main"> 50,00</b></p>
+                </div>
+            </div>
+            <div id="grid-item-main-9">
+                <img src="{{ URL::asset('assets/img/Mini Perfumes/A09.png'); }}" alt="" id="img-main">
+                <div id="mt-20">
+                    Lorem ipsum dolor, sit
+                    <p>R$<b id="title-preco-main"> 50,00</b></p>
+                </div>
+            </div>
+        </div> 
+    <hr>    
+    </main>
+    <footer>
+        <div  id="rodape-main">
+            <nav>
+                <div>
+                    <ul id="rodape-main-ul">
+                        <li><a href="" id="rodape-main-link">Contato</a></li>
+                        <li><a href="" id="rodape-main-link">Termo de serviço</a></li>
+                        <li><a href="" id="rodape-main-link">Política de privacidade</a></li>
+                        <li><a href="" id="rodape-main-link">Cancelamento, troca e reembolso</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <div id="mt-20">
+                <label for="input">Newsletter</label>
+                <input type="email" id="input"placeholder="Digite seu melhor e-mail">
+                <button id="btn">Inscrever</button>
+            </div>
+        </div>
+        <hr>
+
+        <div id="rodape-info">
+            <div>
+                <a href="" id="rodape-info-linkIcon"><i class="fa-brands fa-instagram"></i></a>
+                <a href="" id="rodape-info-linkIcon"><i class="fa-brands fa-square-facebook"></i></a>
+            </div>
+            <div>
+                <a href="" id="rodape-info-linkIcon"><i class="fa-brands fa-cc-mastercard"></i></a>
+                <a href="" id="rodape-info-linkIcon"><i class="fa-brands fa-cc-visa"></i></a>
+                <a href="" id="rodape-info-linkIcon"><i class="fa-brands fa-paypal"></i></a>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
